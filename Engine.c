1f07d24a754491a1e1462194cd6d1a34050c2b21
AlexanderPatapoff <apatapo@uoregon.edu>
2018-06-07 17:16:39 -0400

    expanding stuff

--- Engine.c
+++ Engine.c
@@ -1,10 +1,14 @@
+#include "ArrayHandler.c"
 
 
-#this is an update
 
 
 
-void Engine(){
 
 
+void Engine(int count){
+
+  ArrayHandler <int>test(10);
+  test.AssignValue(14,0);
+  cout << test.GetValue(0) << endl;
 }
---
+++ Entry.c
@@ -0,0 +1,6 @@
+#include "Engine.c"
+
+void Entry()
+{
+  Engine(10);
+}
