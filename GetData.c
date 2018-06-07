e994f156921e12c3c767612fa887147c0e3c771a
AlexanderPatapoff <apatapo@uoregon.edu>
2018-06-07 03:47:05 -0400

    Has Plotting and fitting functionality in the x and y planes TODO: 1.add flexibility 2.automate 3.clean up 4.watch for leaks 5.Integrate OOP

--- GetData.c
+++ GetData.c
@@ -1,6 +1,5 @@
 #include "Engine.c"
 
-
 //structure meant to hold data on current scan info TODO: make lumData non static array size
 struct ID{
   Float_t scanRun;
@@ -9,27 +8,57 @@ struct ID{
   char plane;
   Float_t planeCoord;
   int beamNumber;
-  Int_t lumData [3564];
+  Float_t lumData [3564];
 
 };
 
 //Graph the IDS   ****NOT MODULABLE**** TODO: ADD FLEXIBILITY
-void GraphIDs(Int_t data[]){
-  TCanvas *c1 = new TCanvas("c1","A Simple Graph Example",200,10,500,300);
-  Double_t x[5], y[5];
-  for (Int_t i=0;i<64000;i++) {
-    if(data[i] !=0){
-      y[data[i]] +=1;
-
+void GraphIDs(Float_t data1[][3],Float_t data2[][3], ID beam1[],ID beam2[]){
+  Double_t x[9], y[9],zy[9],zx[9];
+  Double_t x2[9], y2[9],zy2[9],zx2[9];
+    for (size_t i = 0; i < 9; i++) {
+      y[i] = beam1[i].planeCoord-beam2[i].planeCoord;
+      zy[i] = data1[i][0] ;
+    }
+    for (size_t i = 9; i < 18; i++) {
+      x[i-9] = beam1[i].planeCoord-beam2[i].planeCoord;
+      zx[i-9] = data1[i][0] + data1[i][1] + data1[i][2];
+      zx[i-9] += data2[i][0] + data2[i][1] + data2[i][2];
     }
 
-  }
-  for (size_t i = 0; i < 5; i++) {
-    x[i] = i;
-  }
-  TGraph* gr = new TGraph(5,x,y);
-  gr->SetFillColor(40);
-  gr->Draw("AB");
+
+    TCanvas *cx = new TCanvas("cx","xPlaneCollisions",200,10,500,300);
+    TGraph* gx = new TGraph(9,x,zx);
+    TF1 *f1 = new TF1("f1","gaus",-0.1,0.1);
+    gx->Fit("f1","R");
+    gx->Draw("AL");
+    TCanvas *cy = new TCanvas("cy","yPlaneCollisions",200,340,500,300);
+    TGraph* gy = new TGraph(9,y,zy);
+    TF1 *f2 = new TF1("f2","gaus",-0.1,0.1);
+    gy->Fit("f2","R");
+    gy->Draw("AL");
+
+
+
+    // for (size_t i = 0; i < 9; i++) {
+    //   y2[i] = beam2[i].planeCoord;
+    //   zy2[i] = data2[i][0];
+    // }
+    // for (size_t i = 9; i < 18; i++) {
+    //   x2[i-9] = beam2[i].planeCoord;
+    //   zx2[i-9] = data2[i][0];
+    // }
+    //
+    //
+    //
+    // TCanvas *cx2 = new TCanvas("cx2","xPlaneCollisions2",700,10,500,300);
+    // TGraph* gx2 = new TGraph(9,x2,zx2);
+    // gx2->SetFillColor(40);
+    // gx2->Draw("AB");
+    // TCanvas *cy2 = new TCanvas("cy2","yPlaneCollisions2",700,340,500,300);
+    // TGraph* gy2 = new TGraph(9,y2,zy2);
+    // gy2->SetFillColor(40);
+    // gy2->Draw("AB");
 }
 
 //Writes the ID sturct into console
@@ -41,7 +70,17 @@ void printID(ID id){
 //NULL
 void Test(){
   cout <<"test"<< endl;
-  Engine();
+
+
+
+}
+
+
+void compareData(Float_t data1[][3],Float_t data2[][3]){
+
+  for (size_t i = 0; i < 9; i++) {
+    cout<< data1[i][0]<< " " << data2[9-i][0]<< endl;
+  }
 }
 //Entry function of the
 void GetData(){
@@ -50,56 +89,101 @@ void GetData(){
    TTree *tree = (TTree*) filein->Get("vdMScanData;1");
    Int_t nentries = tree->GetEntries();
 
-   ID idEntry [18];
+   ID idEntryB1 [18];
+   ID idEntryB2 [18];
   // List<Int_t[]> totalData;
    cout <<"scanRun scanLB scanStep plane planeCoord" << endl;
    for (size_t i = 0; i < nentries; i++) {
 
      tree->GetEntry(i);
-     idEntry[i].scanRun = tree->GetLeaf("ScanRun")->GetValue(0);
-     idEntry[i].scanLumBlock = tree->GetLeaf("ScanLB")->GetValue(0);
-     idEntry[i].scanStep = tree->GetLeaf("ScanStep")->GetValue(0);
-     if( i >= 8)
+     idEntryB1[i].scanRun = tree->GetLeaf("ScanRun")->GetValue(0);
+     idEntryB1[i].scanLumBlock = tree->GetLeaf("ScanLB")->GetValue(0);
+     idEntryB1[i].scanStep = tree->GetLeaf("ScanStep")->GetValue(0);
+     if( i >= 9)
      {
-       idEntry[i].plane = 'x';
-       idEntry[i].planeCoord = tree->GetLeaf("B1DeltaXSet")->GetValue(0);
+       idEntryB1[i].plane = 'x';
+       idEntryB1[i].planeCoord = tree->GetLeaf("B1DeltaXSet")->GetValue(0);
 
      }else{
-       idEntry[i].plane = 'y';
-       idEntry[i].planeCoord = tree->GetLeaf("B1DeltaYSet")->GetValue(0);
+       idEntryB1[i].plane = 'y';
+       idEntryB1[i].planeCoord = tree->GetLeaf("B1DeltaYSet")->GetValue(0);
      }
 
      Int_t nBaskets = tree->GetLeaf("lucBiHitOR_BunchInstLumi")->GetLen();
 
 
      for (size_t s = 0; s < nBaskets; s++) {
-       idEntry[i].lumData[s] = tree->GetLeaf("lucBiHitOR_BunchInstLumi")->GetValue(s);
+       idEntryB1[i].lumData[s] = tree->GetLeaf("lucBiHitOR_BunchInstLumi")->GetValue(s);
+     }
+     printID(idEntryB1[i]);
+   }
+
+
+    cout <<"-----------------------------------------" << endl;
+    cout <<"scanRun scanLB scanStep plane planeCoord" << endl;
+   for (size_t i = 0; i < nentries; i++) {
+
+     tree->GetEntry(i);
+     idEntryB2[i].scanRun = tree->GetLeaf("ScanRun")->GetValue(0);
+     idEntryB2[i].scanLumBlock = tree->GetLeaf("ScanLB")->GetValue(0);
+     idEntryB2[i].scanStep = tree->GetLeaf("ScanStep")->GetValue(0);
+     if( i >= 9)
+     {
+       idEntryB2[i].plane = 'x';
+       idEntryB2[i].planeCoord = tree->GetLeaf("B2DeltaXSet")->GetValue(0);
+
+     }else{
+       idEntryB2[i].plane = 'y';
+       idEntryB2[i].planeCoord = tree->GetLeaf("B2DeltaYSet")->GetValue(0);
      }
 
+     Int_t nBaskets = tree->GetLeaf("lucBi2HitOR_BunchInstLumi")->GetLen();
 
 
-     printID(idEntry[i]);
-   }
+     for (size_t s = 0; s < nBaskets; s++) {
+       idEntryB2[i].lumData[s] = tree->GetLeaf("lucBi2HitOR_BunchInstLumi")->GetValue(s);
+     }
+
+     printID(idEntryB2[i]);
+
+ }
    //GraphIDs(idEntry[4].lumData);
-   Int_t total [64152];
-   Int_t x =0;
-   for(int i =0; i < 18; i++){
-     for (size_t p = 0; p < 3564; p++) {
-       total[x] = idEntry[i].lumData[p];
-       x = x+1;
+
+
+   //cout << tree->GetLeaf("B1BCIDs")->GetValues() << endl;
+
+  Int_t nBaskets = tree->GetLeaf("B1BCIDs")->GetLen();
+
+  Float_t output1 [18][3];
+
+  for (size_t p = 0; p < nBaskets; p++) {
+     Int_t temp = tree->GetLeaf("B1BCIDs")->GetValue(p);
+
+     for (size_t l = 0; l < 18; l++){
+       output1 [l][p] = idEntryB1[l].lumData[temp];
      }
+
    }
-   GraphIDs(total);
+
+   nBaskets = tree->GetLeaf("B2BCIDs")->GetLen();
+
+   Float_t output2 [18][3];
+
+   for (size_t p = 0; p < nBaskets; p++) {
+      Int_t temp = tree->GetLeaf("B2BCIDs")->GetValue(p);
+
+      for (size_t l = 0; l < 18; l++){
+        output2 [l][p] = idEntryB2[l].lumData[temp];
+      }
+
+    }
 
 
 
-/* attempt
-t1->SetBranchAddress("px",&px);
-   t1->SetBranchAddress("py",&py);
-   t1->SetBranchAddress("pz",&pz);
-   t1->SetBranchAddress("random",&random);
-   t1->SetBranchAddress("ev",&ev);
-*/
+
+  GraphIDs(output1,output2, idEntryB1,idEntryB2);
+  //compareData(output1, output2);
+
 
    filein->Close();
 
